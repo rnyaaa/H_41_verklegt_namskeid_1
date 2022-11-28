fb89e0d84c51b01cb035ef94977fc8d52855c88e
Sigurdur Ingi <siggi101@hotmail.com>
2022-11-28 13:30:21 +0000

    i cri evertim

---
+++ RU Basement Open/logic/IO_API.py
@@ -0,0 +1,23 @@
+def getPlayers():
+    raise NotImplementedError
+
+def getTeams():
+    raise NotImplementedError
+
+def getTournaments():
+    raise NotImplementedError
+
+def getGames():
+    raise NotImplementedError
+
+def getPlayerList():
+    raise NotImplementedError
+
+def getPlayerScore():
+    raise NotImplementedError
+
+def getPlayerScoreByDate():
+    raise NotImplementedError
+
+def getTournamentScores():
+    raise NotImplementedError
