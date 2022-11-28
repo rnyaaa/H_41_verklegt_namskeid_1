43a3f74a8cfa795d0afda35eda176e62b606cad0
rnyaaa <113616131+rnyaaa@users.noreply.github.com>
2022-11-28 14:51:19 +0000

    Update IO_API.py

--- RU Basement Open/IO/IO_API.py
+++ RU Basement Open/IO/IO_API.py
@@ -1,9 +1,9 @@
-class IO_API(playerupdate, teamupdate, tournamentupdate,newresults):
-    def __self__(playerupdate, teamupdate, tournamentupdate, newresults):
-        self.playerupdate = playerupdate
+class IO_API:
 
     def getAllPlayers():
         playerstream = playerIO.loadPlayersFile()
+        for line in playerstream:
+            print(line)
         return playerstream
 
     def getAllTeams():
@@ -22,131 +22,69 @@ class IO_API(playerupdate, teamupdate, tournamentupdate,newresults):
         resultstream = resultsIO.loadResultsFile()
         return resultstream
 
-    def updatePlayers():
-        raise NotImplementedError
+    def updatePlayers(playerupdate):
+        playerIO.storePlayersFile(playerupdate)
 
-    def updateTeams():
-        raise NotImplementedError
+    def updateTeams(teamupdate):
+        teamIO.storeTeamsFile(teamupdate)
 
-    def updateGames():
-        raise NotImplementedError
+    def updateGames(gameupdate):
+        gameIO.storeGamesFile(gameupdate)
 
-    def updateTournaments():
-        raise NotImplementedError
+    def updateTournaments(tournamentupdate):
+        tournamentIO.storeTournamentFile(tournamentupdate)
 
+    def updateResults(newresults, resultsID):
+        resultsIO.storeResultsFile(newresults,resultsID)
 
-class playerIO():
+class playerIO:
     def loadPlayersFile():
-        playerstream = open("/models/players.csv", "r")
+        playerstream = open("/home/ronja/Downloads/Pilukast/models/players.csv", "r")
         return playerstream
 
-    def storePlayersFile():
-        raise NotImplementedError
+    def storePlayersFile(playerupdate):
+        append_player = open("/home/ronja/Downloads/Pilukast/models/players.csv", "a")
+        append_player.write(playerupdate)
+        append_player.close()
 
-class teamIO():
+class teamIO:
     def loadTeamsFile():
         teamstream = open("/models/teams.csv", "r")
         return teamstream
 
-    def storeTeamsFile():
-        raise NotImplementedError
+    def storeTeamsFile(teamupdate):
+        append_teams = open("models/teams.csv", "a")
+        append_teams.write(teamupdate)
+        append_teams.close()
 
-class gameIO():
+class gameIO:
     def loadGamesFile():
         gamestream = open("/models/games.csv", "r")
         return gamestream
 
-    def storeGamesFile():
-        raise NotImplementedError
+    def storeGamesFile(gamesupdate):
+        append_games = open("models/games.csv", "a")
+        append_games.write(gamesupdate)
+        append_games.close()
 
-class tournamentIO():
+class tournamentIO:
     def loadTournamentFile():
         tournamentstream = open("/models/tournaments.csv", "r")
         return tournamentstream
 
-    def storeTournamentFile():
-        raise NotImplementedError
+    def storeTournamentFile(tournamentupdate):
+        append_tournament = open("models/teams.csv", "a")
+        append_tournament.write(tournamentupdate)
+        append_tournament.close()
 
-class resultsIO():
+class resultsIO:
     def loadResultsFile():
         resultstream = open("/models/results.csv", "r")
         return resultstream
 
-    def storeResultsFile():
-        raise NotImplementedError
-class IO_API(playerupdate, teamupdate, tournamentupdate,newresults):
-    def __self__(playerupdate, teamupdate, tournamentupdate, newresults):
-        self.playerupdate = playerupdate
-
-    def getAllPlayers():
-        playerstream = playerIO.loadPlayersFile()
-        return playerstream
-
-    def getAllTeams():
-        teamstream = teamIO.loadTeamsFile()
-        return teamstream
-
-    def getAllGames():
-        gamestream = gameIO.loadGamesFile()
-        return gamestream
-
-    def getAllTournaments():
-        tournamentstream = tournamentIO.loadTournamentFile()
-        return tournamentstream
-
-    def getResults():
-        resultstream = resultsIO.loadResultsFile()
-        return resultstream
-
-    def updatePlayers():
-        raise NotImplementedError
-
-    def updateTeams():
-        raise NotImplementedError
-
-    def updateGames():
-        raise NotImplementedError
-
-    def updateTournaments():
-        raise NotImplementedError
-
-
-class playerIO():
-    def loadPlayersFile():
-        playerstream = open("/models/players.csv", "r")
-        return playerstream
-
-    def storePlayersFile():
-        raise NotImplementedError
-
-class teamIO():
-    def loadTeamsFile():
-        teamstream = open("/models/teams.csv", "r")
-        return teamstream
-
-    def storeTeamsFile():
-        raise NotImplementedError
-
-class gameIO():
-    def loadGamesFile():
-        gamestream = open("/models/games.csv", "r")
-        return gamestream
-
-    def storeGamesFile():
-        raise NotImplementedError
-
-class tournamentIO():
-    def loadTournamentFile():
-        tournamentstream = open("/models/tournaments.csv", "r")
-        return tournamentstream
-
-    def storeTournamentFile():
-        raise NotImplementedError
-
-class resultsIO():
-    def loadResultsFile():
-        resultstream = open("/models/results.csv", "r")
-        return resultstream
-
-    def storeResultsFile():
-        raise NotImplementedError
+    def storeResultsFile(newresults, resultsID):
+        create_new_results = open(resultsID.csv, "x")
+        create_new_results.close()
+        write_new_results = open(resultsID.csv, "w")
+        write_new_results.write(newresults)
+        write_new_results.close()
