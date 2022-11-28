9404f54de8b232170ff882fa3493a634e36449c6
Rebekkaloga <112619790+Rebekkaloga@users.noreply.github.com>
2022-11-28 15:29:39 +0000

    eyða óþarfa fælum

--- RU Basement Open/logic/Results.py
+++
@@ -1,9 +0,0 @@
-class ResultsLL():
-    def getResults():
-        raise NotImplementedError
-
-    def changeResults():
-        raise NotImplementedError
-
-    def updateResults():
-        raise NotImplementedError
