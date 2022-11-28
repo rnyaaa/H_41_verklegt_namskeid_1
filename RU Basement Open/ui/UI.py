fdf0fee9fa3c09af2d53803e9a136703b69d98e4
Sigurdur Ingi <siggi101@hotmail.com>
2022-11-28 14:54:51 +0000

    I-I-I wanna go-o-o all the way-ay-ay (Spears,2011)

---
+++ RU Basement Open/ui/UI.py
@@ -0,0 +1,54 @@
+
+class Main_Menu():
+
+    def displayMainMenu():
+        raise NotImplementedError
+
+    def openOrganizerMenu():
+        raise NotImplementedError
+
+    def openViewerMenu():
+        raise NotImplementedError
+
+    def openCaptainMenu():
+        raise NotImplementedError
+
+    def openShowGamesMenu():
+        raise NotImplementedError
+
+    def language():
+        # C-Requirement functionality to be implemented
+        raise NotImplementedError
+
+
+class ShowGamesUI():
+
+    def showGamesPage():
+        raise NotImplementedError
+
+    def showTournamentDates():
+        raise NotImplementedError
+
+    def showGamesFinished():
+        raise NotImplementedError
+
+    def showUpcomingGames():
+        raise NotImplementedError
+
+
+class viewerUI:
+
+    def displayViewer():
+        None
+
+    def showTournamentInfo():
+        None
+
+    def showTeamViewer():
+        None
+
+    def showPlayerViewer():
+        None
+
+    def showPlayerHighscoreViewer():
+        None
