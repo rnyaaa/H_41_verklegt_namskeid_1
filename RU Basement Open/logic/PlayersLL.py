064e31286d87aa73e516f66d7ed3d77d83a6fc93
Rebekkaloga <112619790+Rebekkaloga@users.noreply.github.com>
2022-11-29 15:13:55 +0000

    Update PlayersLL.py

--- RU Basement Open/logic/PlayersLL.py
+++ RU Basement Open/logic/PlayersLL.py
@@ -24,18 +24,30 @@ class PlayersLL():
         for playerline in playerstream:
 
             player in players                if item == playerline[0] and resultline[item+1] > playerline[5]:
-                    playerlplayeresu                    playerhange player[5] + yerline)
+                    playerlplayeresu                    playerhange int(player[5]) + int(yerline))
         for playerline in play
         resultline = resultstream.readlines()[1].split(",")erstream.split(","):
-            player in players                if item == playerlr
+            player in players:                if item == playerlr
         """ uppfæra Útskot"""
 
 
+                    player[7] = resultline[item+1]
 
         resultline = resultstream.readlines()[2].split(",")
-        for player in players
+        for player in players:
             for item in resultline:
                 if item == player[0] and resultline[item+1] > playerline[6]:
-                    player
+                    player[6] = resultline[item+1]
+        """ uppfæra win ratio, 501 einmenn"""
+        resultline = resultstream.readlines()[7:11]
+        for player in players:
+            for line in resultline:
+                for item in resultline:
+                    if item == player[0]:
+                        player[10] = int(player[10]) + 1
+
+        """ uppfæra win ratio, 301 duo"""
+        """ uppfæra win ratio, cricket"""
+        """ uppfæra win ratio, 501 fjormenn"""
 
 
