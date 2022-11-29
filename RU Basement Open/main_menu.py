5cd1bf295323303db1d83be0bcade252b61bac50
Rebekkaloga <112619790+Rebekkaloga@users.noreply.github.com>
2022-11-29 14:12:17 +0000

    vs code

--- RU Basement Open/main_menu.py
+++ RU Basement Open/main_menu.py
@@ -4,20 +4,32 @@ from ui.UI import Main_Menu, OrganizerUI
 
 user_input = Main_Menu.displayMainMenu()
 if user_input == "1":
-    Main_Menu.openOrganizerMenu()
-elif user_input == "2":
-    selection = Main_Menu.openCaptainMenu()
+    selection = Main_Menu.openOrganizerMenu()
     if selection == "1":
         OrganizerUI.addTeamPage()
     elif selection == "2":
-        OrganizerUI.addTournament
+        OrganizerUI.addTournament()
+    elif selection == "3":
+        OrganizerUI.addPlayer()
+    elif selection == "4":
+        OrganizerUI.changeTournamentDates()
+    elif selection == "5":
+        OrganizerUI.changeResults()
+    else:
+        ("Ekki gildur valmöguleiki!") # Á eftir að útfæra loopu
+
+elif user_input == "2":
+    selection = Main_Menu.openCaptainMenu()
+
 elif user_input == "3":
     Main_Menu.openShowGamesMenu()
+
 elif user_input == "4":
     Main_Menu.openViewerMenu()
+
 elif user_input.lower() == "q":
     print()
     print("Bless 🥲")
     quit()
 else: 
-    print("Ekki gildur valmöguleiki! Reyndu aftur")
+    print("Ekki gildur valmöguleiki! Reyndu aftur") # Á eftir að útfæra loopu
