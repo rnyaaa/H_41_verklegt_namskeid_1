3b179f05bb34cb7ff08bdfce4412e7db61c3a9df
SiggiAtliG <112619955+SiggiAtliG@users.noreply.github.com>
2022-12-01 15:20:12 +0000

    j

--- RU Basement Open/main.py
+++ RU Basement Open/main.py
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
+=======
+from ui.UI import Main_menu_ui, OrganizerUI
+from ui.UI import CaptainUI
+>>>>>>> Stashed changes
 from ui.Main_menu_ui import Main_Menu_UI
 
 mainmenu = Main_Menu_UI()
