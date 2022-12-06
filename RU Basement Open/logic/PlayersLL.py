458b54a5c8eb42ef5435ec13bb01ca53fd14096c
Ronja Johannsdottir <ronjajohannsdottir@rfc1918.rhi.hi.is>
2022-12-06 15:19:54 +0000

    Gerði playerscore föllin
    Gerði líka playersummary model til að auðvelda að fá individual eða
    ranked playerscores.

--- RU Basement Open/logic/PlayersLL.py
+++ RU Basement Open/logic/PlayersLL.py
@@ -1,6 +1,7 @@
 from IO.IO_API import IO_API
 from models.player import Player
 from models.playerscore import PlayerScore
+from models.playersummary import PlayerSummary
 
 class PlayersLL:
 
@@ -20,15 +21,55 @@ class PlayersLL:
         """ adds a player """
         self.ioapi.create_model(player)
 
-    def getPlayerScoreSummarized(self) -> list[PlayerScore]:
-        playerscores = self.getAllPlayerScore
-        players = []
+    def getPlayerScoresSummarized(self) -> list[PlayerSummary]:
+        playerscores = self.getAllPlayerScore()
+        """ get all PlayerScores in dict with the playerid as a key for each of their respective playerscores """
+        playerscores_by_player = dict()
         for item in playerscores:
-            if item[0] not in players:
-                players.append(item[0])
-            if item[0] in players:
-                players[item][1] = players[item]
+            if item.playerid in playerscores_by_player:
+                playerscores_by_player[item.playerid].append(item)
+            else:
+                playerscores_by_player[item.playerid] = [item]
+        
+        player_summaries = []
+        for player_id, scores in playerscores_by_player:
+            """ get PlayerSummary with default values """
+            player_summary = PlayerSummary(player_id)
+            for score in scores:
+                player_summary.QPs += score.QPs
+                player_summary.inshots = max(player_summary.inshots, score.inshots)
+                player_summary.outshots = max(player_summary.outshots, score.outshots)
+                player_summary.result501singles[0] += score.result501singles[0]
+                player_summary.result501singles[1] += score.result501singles[1]
+                player_summary.result301[0] += score.result301[0]
+                player_summary.result301[1] += score.result301[1]
+                player_summary.resultcricket[0] += score.resultcricket[0]
+                player_summary.resultcricket[1] += score.resultcricket[1]
+                player_summary.result501fours[0] += score.result501fours[0]
+                player_summary.result501fours[1] += score.result501fours[1]
+            player_summaries.append(player_summary)
+        return player_summaries
 
+    def getSinglePlayerScore(self, playerid) -> PlayerSummary:
+        """ gets the PlayerSummary of a single player by playerid """
+        player_summaries = self.getPlayerScoresSummarized()
+        for player_summary in player_summaries:
+            if player_summary.playerid == playerid:
+                return player_summary
+
+    def getPlayersSorted(self, sortBy):
+        """ get player list sorted by one of the functions below. You need to replace "sortBy" with one of the functions below """
+        """ e.g. calling this function as getPlayersSorted(ByQualityPoints())"""
+        return sorted(self.getPlayerScoresSummarized(), key=sortBy)
+
+    def ByQualityPoints(player_summary: PlayerSummary):
+        return player_summary.QPs
+
+    def ByInshots(player_summary: PlayerSummary):
+        return player_summary.inshots
+
+    def ByOutshots(player_summary: PlayerSummary):
+        return player_summary.outshots
 
     def getPlayerScoreByDate():
         raise NotImplementedError
